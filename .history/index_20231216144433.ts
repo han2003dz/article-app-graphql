cf0d36f57a44c37c2d5987b0fcc428f93f56b39f
han2003dz <han20031605@gmail.com>
2023-12-16 14:48:12 +0700

    setup project

--- .history/index_20231216144433.ts
+++ .history/index_20231216144433.ts
@@ -1,11 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import * as database from "./config/database";
 
 dotenv.config();
 
 database.connect();
-
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
